8cf79d73698a16319ba8ab51d003b7aaa7fd928c
voidrealms <bcairns@voidrealms.com>
2020-03-10 17:28:24 -0400

    first commit

---
+++ test.cpp
@@ -0,0 +1,18 @@
+#include "test.h"
+
+Test::Test(QObject *parent) : QObject(parent)
+{
+
+}
+
+int Test::number()
+{
+    //Return a number
+    qInfo() << "Called number()";
+    return 42;
+}
+
+void Test::bark()
+{
+    qInfo() << "Bark Bark Bark";
+}
